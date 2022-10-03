7e7c563ae0963935500d525525117d69aacac4d9
rlw92 <r_l_williams@yahoo.com>
2022-10-03 15:20:31 +0100

    General save deploy

--- pokestore-direct/src/RouteSwitch.js
+++ pokestore-direct/src/RouteSwitch.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import App from "./App";
 import Homepage from "./Components/Homepage";
 import ProductsClass from './Components/ProductsClass'
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter basename='/pokestore-direct'>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/products" element={<ProductsClass />} />
 
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
